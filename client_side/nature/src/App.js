d14700fb1c81692861ae7aeb84a0fd833a53d615
HelloMati <118129617+HelloMati@users.noreply.github.com>
2023-03-30 12:19:03 +0100

    Added more css and to .js

--- client_side/nature/src/App.js
+++ client_side/nature/src/App.js
@@ -5,21 +5,22 @@ import { useState } from "react"
 function App() {
   return (
 
-  <header>
-    <nav>
-      <span className="nav_bar">
-        <div className="toggle_button" >
-          <MenuIcon />
-        </div>
-        <ul className="links">
-          <li><a href="#"> Forest</a></li>
-          <li><a href="#"> Ocean</a></li>
-          <li><a href="#"> Rainforest</a></li>
-        </ul>
-        <button>Add a Forest</button>
-      </span>
-    </nav>
-  </header>
+    <header>
+      <nav>
+        <span className="nav_bar">
+          <div className="toggle_button" >
+            <MenuIcon />
+          </div>
+          <ul className="links">
+            <li><a href="#"> Forest</a></li>
+            <li><a href="#"> Ocean</a></li>
+            <li><a href="#"> Rainforest</a></li>
+          </ul>
+          <button className="donate_button">Add a Forest</button>
+        </span>
+      </nav>
+    </header>
+
   );
 }
 
