40d72d61926e1cedaebab2355ad979cb622e3110
W-Sanches94 <117460587+W-Sanches94@users.noreply.github.com>
2023-03-31 16:06:40 +0100

    images added

--- client_side/nature/src/App.js
+++ client_side/nature/src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useState } from "react";
 import AudioPlayer from './components/AudioPlayer';
-
+import YellowStone_img_1 from './background_images/Forest/YellowStone/YellowStone_img_1.jpg';
+import YellowStone_img_2 from './background_images/Forest/YellowStone/YellowStone_img_2.jpg'; 
+import YellowStone_img_3 from './background_images/Forest/YellowStone/YellowStone_img_3.jpg'; 
+import YellowStone_img_4 from './background_images/Forest/YellowStone/Yellowstone_img_4.jpg';   
 
 function App() {
 
@@ -44,7 +47,25 @@ function App() {
     </header>
 
     <body>
-    <AudioPlayer />
+      <AudioPlayer />
+      
+      <section className="container">
+        <div className="slider_wrapper">
+          <div className='slider'>
+            <img id="slide_1" src={YellowStone_img_1} alt="YellowStone forest image 1" />
+            <img id="slide_2" src={YellowStone_img_2} alt="YellowStone forest image 2" />
+            <img id="slide_3" src={YellowStone_img_3} alt="YellowStone forest image 3" />
+            <img id="slide_4" src={YellowStone_img_4} alt="YellowStone forest image 4" />
+          </div>
+
+          <div className="Slider_nav">
+            <a href="#slide_1"></a>
+            <a href="#slide_1"></a>
+            <a href="#slide_1"></a>
+            <a href="#slide_1"></a>
+          </div>
+        </div>
+      </section>
     </body>
     </>
 
