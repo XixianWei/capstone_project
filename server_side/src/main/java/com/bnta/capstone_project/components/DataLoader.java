a79461ae3f69bb4da36ee6b6a810cc1355a5930a
XixianWei <xixian.wei94@gmail.com>
2023-04-03 16:30:33 +0100

    edited data

--- server_side/src/main/java/com/bnta/capstone_project/components/DataLoader.java
+++ server_side/src/main/java/com/bnta/capstone_project/components/DataLoader.java
@@ -40,19 +40,20 @@ public class DataLoader implements ApplicationRunner {
         forestListRepository.save(forests);
 
         //Forest
-        Forest yellowStone = new Forest("Yellow Stone","USA","add information", forests);
+        Forest yellowStone = new Forest("Yellowstone","USA","add information", forests);
         forestRepository.save(yellowStone);
 
         Forest plateau = new Forest("Plateau des Tailles", "Houffalize, BELGIUM", "Martin Dellicour\n" +
-                "    June 2019 - Early evening on the edge of a wetland in middle of the forest. A wild valley populated by beavers in the Belgian Ardennes...",forests);
+                "info  ",forests);
         forestRepository.save(plateau);
 
         Forest haguro = new Forest("Mt Haguro Forest", "Tsuruoka, JAPAN","Mat Eric Hart\n" +
-                "        Summer. 17th August 2019. Recording of Mt. Haguro forest ambience standing in front of the ancient Grandpa cedar tree known as Jijisugi (爺杉). The flow of the Haraigawa river can be heard amongst the evening chorus of the various forest wildlife including the evening cicada known as higurashi (茅蜩) (tanna japonensis), and the large brown cicada known as abura-zemi (油蝉) (graptopsaltria nigrofuscata).",forests );
+                "info   ",forests );
         forestRepository.save(haguro);
 
-        Forest gaio = new Forest("Mirante Gaio","Mirante Gaio, Mato Perso, Rio Grande do Sul, BRAZIL", "Felipe Giongo Krewer\n" +
-                "        The recording was made at 10:30 AM, this place is a belvedere called \"Mirante Gaio\", in this place we can see the river Rio das Antas and his turns.\n", forests);
+        Forest gaio = new Forest("Mirante Gaio","Rio Grande do Sul, BRAZIL", "Felipe Giongo Krewer\n" +
+                " info", forests);
+        forestRepository.save(gaio);
 
     }
 
