e960147a8757f16ef067464125fc7d76da99323a
XixianWei <xixian.wei94@gmail.com>
2023-04-04 15:35:04 +0100

    edited forestSlider

--- client_side/nature/src/App.js
+++ client_side/nature/src/App.js
@@ -8,6 +8,7 @@ import getForestDataWithMedia from './components/ForestData';
 
 function App() {
   const [currentForestData, setCurrentForestData] = useState([]);
+  const [selectedForestId, setSelectedForestId] = useState(1);
 
 useEffect(() => {
   const fetchData = async () => {
@@ -19,13 +20,19 @@ useEffect(() => {
 
 const [currentSlideUrl, setCurrentSlideUrl] = useState(
   currentForestData.length > 0
-    ? currentForestData.find((forest) => forest.id === 1).images[0].url
+    ? currentForestData.find((forest) => forest.id === selectedForestId).images[0].url
     : ""
 );
 
 const handleSlideChange = (newIndex) => {
-  setCurrentSlideUrl(currentForestData.find((forest) => forest.id === 1).images[newIndex].url);
+  setCurrentSlideUrl(currentForestData.find((forest) => forest.id === selectedForestId).images[newIndex].url);
 };
+
+useEffect(() => {
+  if (currentForestData.length > 0) {
+    setCurrentSlideUrl(currentForestData.find((forest) => forest.id === selectedForestId).images[0].url);
+  }
+}, [selectedForestId, currentForestData]);
 
   useEffect(() => {
     document.body.style.backgroundImage = `url(${currentSlideUrl})`;
@@ -34,9 +41,9 @@ const handleSlideChange = (newIndex) => {
   return (
     <>
       <NavBar />
-      <MusicPlayer currentForestData={currentForestData}/>
+      <MusicPlayer currentForestData={currentForestData} onSelectedForestIdChange={setSelectedForestId}/>
       <ForestContainer
-        slides={currentForestData.find((forest) => forest.id === 1)?.images || []}
+        slides={currentForestData.find((forest) => forest.id === selectedForestId)?.images || []}
         onSlideChange={handleSlideChange}
       />
       <style>{`
