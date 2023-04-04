d2eb2f4722202dfc346a3a50e0663aea2e0fc3d7
HelloMati <118129617+HelloMati@users.noreply.github.com>
2023-04-04 14:56:02 +0100

    Updates

--- server_side/src/main/java/com/bnta/capstone_project/models/User.java
+++ server_side/src/main/java/com/bnta/capstone_project/models/User.java
@@ -1,47 +1,5 @@
 package com.bnta.capstone_project.models;
 
-
-public User() {
-    }
-    // getters and setters
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getDonation() {
-        return donation;
-    }
-
-    public void setDonation(Long donation) {
-        this.donation = donation;
-    }
-
-    public List<ForestList> getForestLists() {
-        return forestLists;
-    }
-
     public void setForestLists(List<ForestList> forestLists) {
         this.forestLists = forestLists;
     }
