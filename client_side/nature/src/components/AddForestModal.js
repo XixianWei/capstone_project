213a5321dafd529409ab5f1c2285d8ed17e717eb
W-Sanches94 <117460587+W-Sanches94@users.noreply.github.com>
2023-04-06 09:22:25 +0100

    updated modal and audio button position

--- client_side/nature/src/components/AddForestModal.js
+++ client_side/nature/src/components/AddForestModal.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import {Button} from 'react-bootstrap';
-import {Modal} from 'react-bootstrap';
+import {Button, Modal, Form, Col, Row} from 'react-bootstrap';
 
-function AddForestModal({show, setShow, handleClose, handleShow}) {
+
+function AddForestModal({ show, setShow, handleClose, handleShow }) {
 
   return (
     <>
@@ -14,25 +14,81 @@ function AddForestModal({show, setShow, handleClose, handleShow}) {
         <Modal.Header closeButton>
           <Modal.Title>Add a forest</Modal.Title>
         </Modal.Header>
+
         <Modal.Body>
-          
-          <p>Forest Name:</p>
-          
-          <p>Location:</p>
-          <p>Catergory:</p>
-          <p>Audio:</p>
-          <p>Images:</p>
-          <p>A brief description:</p>
-          <p>First Name:</p>
-          <p>Last Name:</p>
-          <p>Email:</p>
-          
+
+          <Form.Group className="mb-1">
+            <Form.Label>Forest Name:</Form.Label>
+            <Form.Control type="text" placeholder="Enter forest name" />
+          </Form.Group>
+
+          <Form.Group className="mb-1">
+            <Form.Label>Location:</Form.Label>
+            <Form.Control type="text" placeholder="Enter Location" />
+          </Form.Group>
+
+          <Form.Group controlId="audio">
+            <Form.Label>Audio:</Form.Label>
+            <Form.Control type="file" accept="audio/*" />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Image:</Form.Label>
+            <Form.Control
+              type="file"
+              accept="image/*"
+              onChange={(event) => {
+                const file = event.target.files[0];
+                // Do something with the file, like upload it to a server
+              }}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Select Category:</Form.Label>
+            <Form.Select aria-label="Select Category">
+              <option>Select a category</option>
+              <option value="Temperate">Temperate</option>
+              <option value="Subtropical">Subtropical</option>
+              <option value="Tropical">Tropical</option>
+              <option value="Boreal">Boreal</option>
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group className="mb-1">
+            <Form.Label>A brief description:</Form.Label>
+            <Form.Control as="textarea" rows={3} placeholder="a brief description"/>
+          </Form.Group>
+
+          <Form.Group className="mb-1">
+            <Form.Label>First Name:</Form.Label>
+            <Form.Control type="text" placeholder="Enter first name" />
+          </Form.Group>
+
+          <Form.Group className="mb-1">
+            <Form.Label>Last Name:</Form.Label>
+            <Form.Control type="text" placeholder="Enter last name" />
+          </Form.Group>
+
+          <Form.Group className="mb-1" controlId="formBasicEmail">
+            <Form.Label>Email address:</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
         </Modal.Body>
+
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
+          <Button variant="primary" type="submit" onClick={handleClose}>
+            Submit
+          </Button>
         </Modal.Footer>
+
       </Modal>
     </>
   );
--- client_side/nature/src/components/AudioDots.js
+++ client_side/nature/src/components/AudioDots.js
@@ -3,7 +3,7 @@ const AudioDots = ({ currentForestData, onDotClick }) => {
     <div className="audio-dots">
       {currentForestData.map((forest, index) => (
         <span key={index} onClick={() => onDotClick(forest.sounds[0], forest.id)}>
-          ◯
+          ⬤
         </span>
       ))}
     </div>
